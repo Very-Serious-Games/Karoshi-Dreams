9802933de53a00a45ad685484be2d9ae17b67723
Hugo Planell <hugoplanellmoreno@gmail.com>
2024-01-29 16:25:56 +0100

    Folio Cambios

---
+++ Project Liminal/Assets/Code/Scripts/PrinterController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PrinterController : MonoBehaviour
+{
+    public GameObject paperPrefab;
+
+    public GameObject textContainer;
+    private TextMeshProUGUI screenText;
+
+    private TextMeshProUGUI thisText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenText = textContainer.GetComponent<TextMeshProUGUI>();
+        thisText = paperPrefab.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Print()
+    {
+        GameObject obj = Instantiate(paperPrefab, transform.position, transform.rotation);
+        TextMeshProUGUI text = obj.folioText;
+        text.text = screenText.text;
+        screenText.text = "";
+    }
+}
