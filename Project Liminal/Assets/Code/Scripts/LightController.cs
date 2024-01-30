e3ea431925fb91244a4345b9ee2400680453f8e5
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2024-01-30 17:36:55 +0100

    fluorescent light script

--- Project Liminal/Assets/Code/Scripts/LightController.cs
+++ Project Liminal/Assets/Code/Scripts/LightController.cs
@@ -1,18 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
 
 public class LightController : MonoBehaviour
 {
+    public float threshold = 0.01f;
+    public float flickerAmount = 0.2f;
+
+    private Light lightComponent;
+    private float originalIntensity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lightComponent = GetComponent<Light>();
+        originalIntensity = 35;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Random.value < threshold)
+        {
+            StartCoroutine(TurnOnLight());
+            StartCoroutine(TurnOffLight());
+        }
+    }
+
+    private IEnumerator TurnOnLight()
+    {
+        float duration = Random.Range(0.1f, 0.5f);
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            float intensity = Mathf.Lerp(0, originalIntensity, elapsed / duration);
+            lightComponent.intensity = intensity + Random.Range(-flickerAmount, flickerAmount);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        lightComponent.intensity = originalIntensity;
+    }
+
+    private IEnumerator TurnOffLight()
+    {
+        float duration = Random.Range(0.1f, 0.5f);
+        float elapsed = 0;
+
+        while (elapsed < duration)
+        {
+            lightComponent.intensity = Mathf.Lerp(originalIntensity, 0, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        lightComponent.intensity = 0;
     }
 }
